0bdec993b9735cd56c4087512d724d68d57a52d8
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-03-28 15:51:35 +0200

    add of tailwind style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result + e.target.value);
+  };
+  const clearDisplay = () => {
+    setResult("");
+  };
+  const calculate = () => {
+    setResult(eval(result).toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex flex-col justify-center items-center">
+      <div class="bg-gray-100 p-8 rounded-md shadow-lg">
+        <h1 class=" flex justify-center text-2xl font-bold mb-4">➕➗✖️🟰</h1>
+        <div className=" mx-auto h-80  flex-col flex justify-center items-center ">
+          <div className="px-1">
+            <div className="border-graay bg-gray-light h-12 text-xl flex justify-start rounded items-center w-52 px-2 ">
+              {result === "" ? 0 : result}
+            </div>
+          </div>
+          <div className="flex">
+            <div className="grid grid-cols-3">
+              <input
+                type="button"
+                value={"9"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"8"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"7"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"6"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"5"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"4"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"3"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"2"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"1"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"0"}
+                className="col-span-2 text-gray-dark border border-gray hover:border-2 h-10 w-20 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"."}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+            </div>
+            <div className="">
+              <input
+                type="button"
+                value={"+"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-orange flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"-"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-orange flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"*"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-orange flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+              <input
+                type="button"
+                value={"/"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-orange flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+            </div>
+            <div>
+              <input
+                type="button"
+                value={"%"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-orange flex justify-center items-center rounded"
+                onClick={handleClick}
+              />
+
+              <input
+                type="button"
+                value={"DEL"}
+                className=" text-gray-dark border border-gray hover:border-2 h-10 w-10 bg-gray-light flex justify-center items-center rounded"
+                onClick={clearDisplay}
+              />
+              <input
+                className=" text-gray-dark border border-gray hover:border-2 h-20 w-10 bg-green flex justify-center items-center rounded"
+                type="button"
+                value={"="}
+                onClick={calculate}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      gold: "#f2dfa5",
+      black: "#000000",
+      white: "#FFFFFF",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      graay: "#464646",
+      "gray-light": "#d3dce6",
+      salmon: "salmon",
+      dark: "rgb(15, 15, 15)",
+      red: "#ff0000",
+    },
+    fontFamily: {
+      oxygen: ["Oxygen", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
